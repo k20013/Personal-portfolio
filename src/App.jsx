745ff3de711a6bbbd23d89fdf3dc3a33d2da00ae
CalebBravo <calebphone72@gmail.com>
2024-03-21 16:23:30 -0300

    Layout del reproductor y el cassette. Correciones varias.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css';
 
 //Importación de componentes
-import Walkman from './components/walkman/walkman';
+import TapePlayer from './components/tape player/tape player';
 
 function App() {
 
   return (
     <>
-      <Walkman />
+      <div className="walkman-container">
+
+        <div className="top-half">
+
+          <div className="tape-player">
+            <TapePlayer />
+          </div>
+
+          <div className="right-section"></div>
+
+        </div>
+
+        <nav></nav>
+
+      </div>
     </>
   )
 }
--- src/components/cassette/cassette.jsx
+++ src/components/cassette/cassette.jsx
@@ -1,25 +1,12 @@
 import styles from './cassette.module.css';
 
-const Cassette = (status) => {
+import cassette from '../../assets/cassette.svg';
+
+const Cassette = () => {
 
     return(
-        <div className={styles.shell}>
-
-            <div className={styles.shellContent}>
-
-                <h1 className={styles.caveat}>Caleb S Bravo</h1>
-
-                <div>
-
-                    <div className={styles.shellWindow}>
-                    
-                    </div>
-
-                </div>
-
-                <span className={styles.zeyada}>Full stack developer</span>
-            </div>
-
+        <div className={styles.cassetteContainer}>
+            <img src={cassette} alt="" />
         </div>
     )
 }
