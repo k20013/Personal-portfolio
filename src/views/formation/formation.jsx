eab81a007ab1b98445e3ca7274a3d96439cf638a
CalebBravo <calebphone72@gmail.com>
2024-04-01 19:43:17 -0300

    Layout de la sección de formación.

--- src/views/formation/formation.jsx
+++ src/views/formation/formation.jsx
@@ -1,10 +1,82 @@
 import styles from './formation.module.css';
 
-const Formation = () => {
+const Formation = ({ idiom }) => {
 
     return(
-        <section className={styles.formationContainer}>
-            <h1>Formation</h1>
+        <section className={styles.formationSection}>
+
+            <div className={styles.formationContainer}>
+                <div className={styles.skillsContainer}>
+                    <h3>{ idiom === "ESP" ? "Habilidades" : "Skills" }</h3>
+
+                    <div className={styles.filesContainer}>
+
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+
+                    </div>
+
+                </div>
+
+                <div className={styles.techFormation}>
+                    <h3>{ idiom === "ESP" ? "Formación" : "Formation" }</h3>
+
+                    <div className={styles.filesContainer}>
+
+                    <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+                        <article>
+                            <h2>Titulo</h2>
+                            <h3>Institucion</h3>
+                            <span>+</span>                            
+                        </article>
+
+                    </div>
+
+                </div>
+            </div>
+
         </section>
     )
 }
