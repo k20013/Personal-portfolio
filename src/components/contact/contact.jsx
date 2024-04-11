5bc520def550309e652e045a42c027a23b3315f7
CalebBravo <calebphone72@gmail.com>
2024-04-11 17:22:34 -0300

    Finished contact buttons.

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,20 +1,27 @@
 import styles from './contact.module.css';
 
-import InstagramBtn from './instagram-btn/instagram-btn';
 import LinkedinBtn from './linkedin-btn/linkedin-btn';
 import WhatssapBtn from './whatssapp-btn/whatssapp-btn';
 import GithubBtn from './github-btn/github-btn';
-//import MailBtn from './mail-btn/mail-btn';
+import MailBtn from './mail-btn/mail-btn';
 
 const Contact = () => {
 
     return(
         <div className={styles.contactContainer}>
-            
-            <InstagramBtn />
-            <LinkedinBtn />
-            <GithubBtn />
-            <WhatssapBtn />
+
+            <a href="https://github.com/k20013" target="_blank">
+                <GithubBtn />
+            </a>
+            <a href="https://www.linkedin.com/in/caleb-bravo-228684204/" target="_blank">
+                <LinkedinBtn />
+            </a>
+            <a href="https://wa.me/+5491162622680" target="_blank">
+                <WhatssapBtn />
+            </a>
+            <a href="mailto:calebphone72@gmail.com?subject=Contact from portfolio">
+                <MailBtn />
+            </a>            
         </div>
     )
 }
