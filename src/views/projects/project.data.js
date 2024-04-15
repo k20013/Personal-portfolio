cdb56dabca9bb0d1c5995c391066c1422f8ed0fe
CalebBravo <calebphone72@gmail.com>
2024-04-15 14:27:53 -0300

    Done personal projects's layout.

---
+++ src/views/projects/project.data.js
@@ -0,0 +1,32 @@
+import iconDawn from '../../assets/lorem-waifus/Dawn.jpg';
+import iconNami from '../../assets/lorem-waifus/Nami.jpg';
+import iconRukia from '../../assets/lorem-waifus/Rukia.jpg';
+import iconSonia from '../../assets/lorem-waifus/Sonia.jpg';
+
+export default [
+    {
+        name: "Dawn",
+        description: "lorem ipsum",
+        img: iconDawn,
+        linkToWebsite: "",
+        linkToGithub: "https://github.com"
+    },{
+        name: "Nami",
+        description: "lorem ipsum",
+        img: iconNami,
+        linkToWebsite: "",
+        linkToGithub: "https://github.com"
+    },{
+        name: "Rukia",
+        description: "lorem ipsum",
+        img: iconRukia,
+        linkToWebsite: "",
+        linkToGithub: "https://github.com"
+    },{
+        name: "Sonia",
+        description: "lorem ipsum",
+        img: iconSonia,
+        linkToWebsite: "",
+        linkToGithub: "https://github.com"
+    }
+];
--- src/views/projects/projects.jsx
+++ src/views/projects/projects.jsx
@@ -1,21 +1,46 @@
 import styles from './projects.module.css';
 
+import projectData from './project.data';
+import vinyl from '../../assets/vinyl-record.svg';
+
 const Projects = () => {
 
-    return(
+    return (
         <section className={styles.projectsSection}>
 
-            <div className={styles.principalContainer}>
-                <div className={styles.numberOne}></div>
-                <div className={styles.numberTwo}></div>
-                <div className={styles.numberThree}></div>
-                <div className={styles.numberFour}></div>
+            <div className={styles.personalProjectsContainer}>
+
+            { projectData.map((project) => {
+                
+                return(
+                    <article className={styles.projectContainer}>
+
+                    <img src={project.img} className={styles.iconImg} alt={project.description} />
+
+                    <img src={vinyl} className={styles.svgImg} alt="" />
+
+                    <div className={styles.content}>
+                        <h1>{project.name}</h1>
+
+                        <p>{project.description}</p>
+
+                        <div>
+                        <a href={project.linkToWebsite}>
+                            <button><span>Website</span></button>
+                        </a>
+
+                        <a href={project.linkToGithub}>
+                            <button><span>Repository</span></button>
+                        </a>
+                        </div>
+                    </div>
+
+                </article>
+                )
+            })}
+
             </div>
-
-            <div className={styles.githubContainer}>
-
-            </div>
-
+            
         </section>
     )
 }
